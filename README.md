50db0d81ae62045bbbb705f557ce7c42fde6934a
tom_tubeless <lutz.tomala@gmail.com>
2021-09-14 16:37:25 +0200

    inital push

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux Server Management
+
+Teaching Linux-Server-Management
---
+++ book/_build/jupyter_execute/docs/000_intro.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Bio am WWG
+# 
+# Unterrichtsplanung, -inhalte und -materialien für das Fach Biologie am Wim-Wenders-Gymnasium Düsseldorf.
